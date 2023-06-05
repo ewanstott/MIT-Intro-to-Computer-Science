7a7dbfc1fda2cc17a38c3a21b8f8940e037fcbb2
Ewan Stott <ewan.stott1@gmail.com>
2023-06-05 13:35:37 +0100

    commiy

---
+++ pset_1_problem_1.py
@@ -0,0 +1,6 @@
+s = 'azcbobobegghakl'
+vowels = 0
+for char in s:
+    if char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u':
+        vowels += 1
+print("Numbers of vowels: ", vowels)
---
+++ unit1_python_basic_exercises.py
@@ -0,0 +1,280 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  1 08:29:23 2023
+
+@author: ewstott
+"""
+# n = input('You are in the Lost Forest. Go left or right?')
+# while n == 'right': 
+#     n = input('You are in the Lost Forest. Go left or right?')
+# print("You got out of the Lost Forest") 
+
+# if happy > 2:
+#     print('hello world')
+    
+# num = 5
+# if num > 2:
+#     print(num)
+#     num -= 1
+# print(num)
+
+# num = 0
+# while num <= 5:
+#     print(num)
+#     num += 1
+    
+# print("Outside of loop")
+# print(num) 
+
+# numberOfLoops = 0
+# numberOfApples = 2
+# while numberOfLoops < 10:
+#     numberOfApples *= 2
+#     numberOfApples += numberOfLoops
+#     numberOfLoops -= 1
+# print("Number of apples: " + str(numberOfApples))
+
+# num = 10
+# while True:
+#     if num < 7:
+#         print('Breaking out of loop')
+#         break
+#     print(num)
+#     num -= 1
+# print('Outside of loop')
+
+# num = 100
+# while not False:
+#     if num < 0:
+#         break
+# print('num is: ' + str(num))
+
+# x = 2
+# while x < 12:
+#     print(x)
+#     x += 2
+# print('Goodbye!')
+
+
+# counter = 0
+# q = 1
+
+# while counter < 6: #end
+#     counter + 1
+#     counter += q
+#     break
+# print(q)
+
+
+# total = 0
+# current = 1
+# while current <= 6:
+#     total += current
+#     current += 1
+    
+# print(total) 
+
+
+# for x in range(2,12,2):
+#     print(x)
+# print('Goodbye!')
+
+
+# print("Hello!")
+# for x in range(10,0,-2):
+#     print(x)
+
+# total = 0
+# current = 1
+
+# for x in range(current,6+1):
+#     total += current
+    
+# print(total)
+        
+# total = 0
+# for next in range(1, 6+1):
+#     total += next
+# print(total)
+
+# end = 6
+# total = end
+# for next in range(end):
+#     total += next
+# print(total) 
+
+# x = 5
+# ans = 0
+# itersLeft = x
+# while (itersLeft != 0):
+#     ans = ans + x
+#     itersLeft = itersLeft - 1
+# print(str(x) + '*' + str(x) + ' = ' + str(ans))
+
+# num = 10
+# for num in range(3):
+#     print(num)
+# print(num)
+
+# divisor = 2
+# for num in range(0, 10, 2):
+#     print(num/divisor)
+
+# for variable in range(20):
+#     if variable % 4 == 0:
+#         print(variable)
+#     if variable % 16 == 0:
+#         print('Foo!')
+
+# for letter in 'hola':
+# #     print(letter)
+    
+# count = 0
+# for letter in 'Snow!':
+#     print('Letter # ' + str(count) + ' is  str(letter))
+#     count += 1
+#     break
+# print(count) 
+    
+# count = 0
+# for letter in 'Snow!':
+#     print('Letter # ' + str(count) + ' is ' + str(letter))
+#     count += 1
+#     break
+# print(count)
+
+# myStr = '6.00x'
+# for char in myStr:
+#     print(char)
+
+# myStr = '6.00x'
+
+# for char in myStr:
+#     print(char)
+
+# print('done')
+
+# greeting = 'Hello!'
+# count = 0
+
+# for letter in greeting:
+#     count += 1
+#     if count % 2 == 0:
+#         print(letter)
+#     print(letter)
+    
+# print('done')
+
+# x = int(input('Enter an integer: '))
+# ans = 0
+# while ans**3 < abs(x):
+#     ans = ans + 1
+# if ans**3 != abs(x):
+#     print(str(x) + ' is not a perfect cube')
+# else: 
+#     if x < 0:
+#         ans = - ans    
+#     print('Cube root of ' + str(x) + ' is ' + str(ans))
+
+# cube = -28
+# for guess in range(abs(cube)+1):
+#     if guess**3 == abs(cube):
+#         break
+# if guess**3 != abs(cube):
+#     print(cube, 'is not a perfect cube')
+# else:
+#     if cube < 0:
+#         guess = -guess
+#     print("Cube root of ", str(cube), " is ", str(guess))
+
+# iteration = 0
+# count = 0
+# while iteration < 5:
+#     for letter in "hello, world":
+#         count += 1
+#     print("Iteration " + str(iteration) + "; count is: " + str(count))
+#     iteration += 1 
+      
+# count = 0
+# phrase = 'hello, world'
+# for iteration in range(5):
+#     count += len(phrase)
+#     print("Iteration " + str(iteration) + "; count is: " + str(count))
+    
+    
+s = 'azcbobobegghakl'
+count = 0
+# len(str(s))
+# print(s)
+
+for letter in (s):
+    count += 1
+print(count)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
