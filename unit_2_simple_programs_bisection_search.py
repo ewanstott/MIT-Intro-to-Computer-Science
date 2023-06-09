efb647bec49023e91bc45826847933491701fceb
Ewan Stott <ewan.stott1@gmail.com>
2023-06-09 08:31:23 +0100

    commit

---
+++ unit_2_simple_programs_bisection_search.py
@@ -0,0 +1,17 @@
+low = 0
+high = 100
+print("Please think of a number between 0 and 100!")
+
+while True:
+    guess = (high + low)//2
+    print("Is your secret number " + str(guess) + "?")
+    ans = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly. ")
+    if ans == 'c':
+        print("Game over. Your secret number was: " + str(guess))
+        break
+    elif ans == 'l': # ans to low
+        low = guess 
+    elif ans == 'h': # ans to high
+        high = guess
+    else:
+        print("Sorry, I did not understand your input.")
